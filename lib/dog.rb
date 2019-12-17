39f38da64179e97e9482ccfdbad2b8719ed46965
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-17 14:55:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Add your code here
+class Dog 
+  
+  @@all = ["Rover", "Fido", "Bane"]
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    
+  rover = Dog.new("Rover")
+  fido = Dog.new("Fido")
+  bane = Dog.new("Bane")
+  
+Dog.all
